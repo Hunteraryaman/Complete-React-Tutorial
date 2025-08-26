1b34c89a1fc3c6a129f70d36dfa5e3bd409f7f76
Hunteraryaman <aryamangmte@gmail.com>
2025-08-26 15:32:45 +0530

    dd
    ss

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,7 +1,7 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Create from './Create';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -9,14 +9,12 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-          </Switch>
+            {/* using routes instead of switch as the notation has changed */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* this is needed as it is a new version of react and the style has changed */}
+            <Route path="/create" element={<Create />} />
+          </Routes>
         </div>
       </div>
     </Router>
